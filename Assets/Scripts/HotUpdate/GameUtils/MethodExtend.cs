11435afc159d24282cd2cb8d6481abadd1df27ec
H-Bole <2934073538@qq.com>
2025-07-23 03:23:48 +0800

    增加拓展方法

--- Assets/Scripts/HotUpdate/GameUtils/MethodExtend.cs
+++ Assets/Scripts/HotUpdate/GameUtils/MethodExtend.cs
@@ -10,16 +10,149 @@ namespace HotUpdate
     /// </summary>
     public static class MethodExtend
     {
+        #region Object拓展,支持链式调用
+        
+        ///快速实例化
+        public static T InstantiateQuick<T>(this T selfObj) where T : Object
+        {
+            return Object.Instantiate(selfObj);
+        }
+
+        ///快速实例化
+        public static T InstantiateQuick<T>(this Object selfObj) where T : Object
+        {
+            return (T)Object.Instantiate(selfObj);
+        }
+
+        ///快速实例化,指定父物体
+        public static T InstantiateQuick<T>(this T selfObj, Transform parent) where T : Object
+        {
+            return Object.Instantiate(selfObj, parent);
+        }
+        
+        ///快速实例化,指定父物体
+        public static T InstantiateQuick<T>(this Object selfObj, Transform parent) where T : Object
+        {
+            return (T)Object.Instantiate(selfObj, parent);
+        }
+
+        ///重命名
+        public static T Name<T>(this T selfObj, string name) where T : Object
+        {
+            selfObj.name = name;
+            return selfObj;
+        }
+
+        ///销毁任意对象
+        public static void DestroySelf<T>(this T selfObj) where T : Object
+        {
+            Object.Destroy(selfObj);
+        }
+
+        ///安全销毁任意对象
+        public static T DestroySelfGracefully<T>(this T selfObj) where T : Object
+        {
+            if ((bool)(Object)selfObj)
+                Object.Destroy(selfObj);
+            return selfObj;
+        }
+
+        ///在一定时间后销毁任意对象
+        public static T DestroySelfAfterDelay<T>(this T selfObj, float afterDelay) where T : Object
+        {
+            Object.Destroy(selfObj, afterDelay);
+            return selfObj;
+        }
+
+        ///在一定时间后安全销毁任意对象
+        public static T DestroySelfAfterDelayGracefully<T>(this T selfObj, float delay) where T : Object
+        {
+            if ((bool)(Object)selfObj)
+                Object.Destroy(selfObj, delay);
+            return selfObj;
+        }
+
+        ///任意对象加锁不销毁
+        public static T DontDestroyOnLoad<T>(this T selfObj) where T : Object
+        {
+            Object.DontDestroyOnLoad(selfObj);
+            return selfObj;
+        }
+
+        ///任意对象转换为指定类型
+        public static T As<T>(this Object selfObj) where T : Object
+        {
+            return selfObj as T;
+        }
+
+        #endregion
+        
+        #region GameObject拓展,支持链式调用
+
+        ///激活GameObject
+        public static GameObject Show(this GameObject target)
+        {
+            target?.SetActive(true);
+            return target;
+        }
+
+        ///激活组件的GameObject
+        public static T Show<T>(this T selfComponent) where T : Component
+        {
+            selfComponent?.gameObject.Show();
+            return selfComponent;
+        }
+
+        ///禁用GameObject
+        public static GameObject Hide(this GameObject target)
+        {
+            target?.SetActive(false);
+            return target;
+        }
+        ///禁用组件的GameObject
+        public static T Hide<T>(this T selfComponent) where T : Component
+        {
+            selfComponent?.gameObject.Hide();
+            return selfComponent;
+        }
+        
+        ///销毁GameObject 
+        public static void DestroyGameObj<T>(this T selfBehaviour) where T : Component
+        {
+            selfBehaviour.gameObject.DestroySelf();
+        }
+
+        ///安全销毁GameObject
+        public static void DestroyGameObjGracefully<T>(this T selfBehaviour) where T : Component
+        {
+            if (!(bool)(Object)selfBehaviour || !(bool)(Object)selfBehaviour.gameObject)
+                return;
+            selfBehaviour.gameObject.DestroySelfGracefully();
+        }
+
+        ///在一定时间后销毁GameObject
+        public static T DestroyGameObjAfterDelay<T>(this T selfBehaviour, float delay) where T : Component
+        {
+            selfBehaviour.gameObject.DestroySelfAfterDelay(delay);
+            return selfBehaviour;
+        }
+
+        ///在一定时间后安全销毁GameObject
+        public static T DestroyGameObjAfterDelayGracefully<T>(this T selfBehaviour, float delay) where T : Component
+        {
+            if ((bool)(Object)selfBehaviour && (bool)(Object)selfBehaviour.gameObject)
+                selfBehaviour.gameObject.DestroySelfAfterDelay(delay);
+            return selfBehaviour;
+        }
+
+        #endregion
+        
         #region DOTween拓展
 
         public static Tween KillTo0(this Tween t)
         {
-            if (t != null)
-            {
-                t.Goto(0, true);
-                t.Kill();
-            }
-
+            t?.Goto(0, true);
+            t?.Kill();
             return null;
         }
 
@@ -35,28 +168,6 @@ namespace HotUpdate
 
         #endregion
 
-        #region GameObject拓展
 
-        public static GameObject Show(this GameObject target)
-        {
-            if (target != null)
-            {
-                target.SetActive(true);
-            }
-
-            return target;
-        }
-
-        public static GameObject Hide(this GameObject target)
-        {
-            if (target != null)
-            {
-                target.SetActive(false);
-            }
-
-            return target;
-        }
-
-        #endregion
     }
 }
